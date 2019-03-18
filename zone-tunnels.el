85db38f550f629d3e7ebe085a2ad85fbbbcebeff
Mohammed Ismail Ansari <team.terminal@gmail.com>
2019-03-18 10:45:25 -0500

    set version to 1.0

--- zone-tunnels.el
+++ zone-tunnels.el
@@ -3,6 +3,7 @@
 ;; This file is not part of Emacs
 
 ;; Author: Mohammed Ismail Ansari <team.terminal@gmail.com>
+;; Version: 1.0
 ;; Keywords: games
 ;; Maintainer: Mohammed Ismail Ansari <team.terminal@gmail.com>
 ;; Created: 2018/03/08
